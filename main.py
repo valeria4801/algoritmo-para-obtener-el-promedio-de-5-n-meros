a43887e429f8034657dd8082b6753bb59732424c
VALERIA RODRIGUEZ <90343758+valeria4801@users.noreply.github.com>
2021-10-06 17:41:42 +0000

    ejecicios de practica de algoritmos

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-Dame el número 1:
-7
-Dame el número 2:
-7
-Dame el número 3:
-7
-Dame el número 4:
-7
-Dame el número 5: 
-7
-Dados los datos, el promerio sera :7 
-
-  
+python 3.7.10 packaged by conda-forge (defaul)
+oct 5 2021, 17:08:41
+gcc 9.4.0 con linux
+Type "help" , "copyright" , "credits"  or "license filter"
+or more information
+>>> frase "estamo>
+>>> frase "estamos en la clase de programacion de tercero B"
+>>> type (frase) 
+<class "str>"
+>>> cali1 =9.5
+>>> type (cali1)
+<class "float">
+>>> cali2 =10
+>>> type (cali2)
+<class "int>"
+>>>17>8
+False
+>>> maxima-cali=10
+>>>9 == maxima-cali1false
+>>>10== maxima-cali1
+True
+>>> respuesta =10 === maxima-cali
+>>> respuesta
+True
+>>> 5+4
+9
+>>> 5.0+4.0
+9.0
+>> 9-4
+5
+>>> 9.5-4
+5.5
+>>>8+7
+56
+>>> 8.5+0.5
+4.25
+>>> 9/3
+3.0
+>>>9/4
+file <stidin> line 1
+9/4
+IndentationError:unexpected IndentationError
+>>> 9/4
+2.25
+>>> residuo=9%4
+>>>res
+residuo respuesta
+>>> residuo
+1
